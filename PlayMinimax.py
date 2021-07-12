1d1a18c3fd012de8510bdbc25e1cafdc62a00328
David2Garcia <58831634+David2Garcia@users.noreply.github.com>
2021-07-12 20:18:42 +0200

    Now it changes the depth when many tiles are occupied to improve the move decision in a difficult momment

--- PlayMinimax.py
+++ PlayMinimax.py
@@ -4,13 +4,13 @@ from Grid import Grid
 from GameDriver import GameDriver
 from Minimax import getBestMove
 from FileName import path_file
-import time
 
 gameDriver = GameDriver()
-datafile = open(path_file('DataMinimaxIV'), "a")
+datafile = open(path_file('DataMinimax'), "a")
 
 moves_str = ['UP', 'DOWN', 'LEFT', 'RIGHT']
 moves_count = 1
+blank = 8
 
 while True:
     grid = gameDriver.getGrid()
@@ -18,8 +18,10 @@ while True:
         datafile.close()
         print("Unfortunately, I lost the game.")
         break
-
-    moveCode = getBestMove(grid, 5)
+    if blank < 5:
+        moveCode = getBestMove(grid, 10)
+    else:
+        moveCode = getBestMove(grid, 5)
 
     print(f'Move #{moves_count}: {moves_str[moveCode]}')
     gameDriver.move(moveCode)
@@ -28,11 +30,15 @@ while True:
         moves_count += 1
         line = '{},'.format(moves_count)
 
+        blank = 0
         for row in grid.matrix:
             for number in row:
                 line += str(number) + ','
+                if number == 0:
+                    blank += 1
 
         line += moves_str[moveCode]
         datafile.write('{}\n'.format(line))
+
 
 
